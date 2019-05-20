d3e536d8567bd983980a2394142064e0f2a63893
Bilel KHARBECHE <bilelkharbeche71@gmail.com>
2019-05-20 14:59:17 +0200

    TP2

--- src/main/java/fr/diginamic/demo_jdbc/TestConnexionJdbc.java
+++ src/main/java/fr/diginamic/demo_jdbc/TestConnexionJdbc.java
@@ -3,18 +3,29 @@ package fr.diginamic.demo_jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class TestConnexionJdbc {
 
 	public static void main(String[] args) {
 
+		ResourceBundle monFichierConf = ResourceBundle.getBundle("database");
+
+		String driverName = monFichierConf.getString("database.driver");
+		String url = monFichierConf.getString("database.url");
+		String userName = monFichierConf.getString("database.user");
+		String password = monFichierConf.getString("database.password");
+
+		try {
+			Class.forName(driverName);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
 		Connection conn = null;
 
 		try {
-			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-			conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/pizzeria?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
-					"root", "lolbilel");
+			conn = DriverManager.getConnection(url, "root", "lolbilel");
 			System.out.println(conn);
 		} catch (SQLException e) {
 			e.printStackTrace();
